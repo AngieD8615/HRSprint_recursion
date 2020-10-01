b4130d781a0aa655d7d8f0521e4d3c1751b9b28f
AngieD8615 <adavidson1@gmail.com>
2020-10-01 09:17:48 -0700

    began base case for stringiy

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,32 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result = '';
+  const helper = (obj) => {
+    if (typeof obj === 'number') {
+      result += obj;
+    } else if (typeof obj === 'string') {
+      result += `"${obj}"`;
+    } else if (typeof obj === 'boolean') {
+      result += `${obj}`;
+    } else if (obj === null) {
+      result += `${null}`;
+    } else if (Array.isArray(obj) && obj.length === 0) {
+      result += '[]';
+    } else if (!Array.isArray(obj) && typeof obj === 'object' && Object.keys(obj).length === 0) {
+      result += '{}';
+    }
+
+
+
+  };
+  helper(obj);
+  return result;
 };
+
+
+var testToString = [1, 2, 'hi', true];
+var testNumToString = 1;
+var testStringToString = 'hello world';
+console.log(stringifyJSON (testNumToString));
+console.log(typeof null);
